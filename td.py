687d286ce684406955f6146f8d23eb97a3210066
Nazia Habib <habib.nazia@gmail.com>
2019-11-30 11:01:16 -0600

    Add files via upload

--- td.py
+++ td.py
@@ -1,51 +1,57 @@
-class TD:
-    def __init__(self, actions, epsilon=0.1, alpha=0.2, gamma=0.9):
-        self.q = {}
-        self.actions = [i for i in range(actions)]
 
-    def getQ(self, state, action):
-        return self.q.get((state, action), 0.0)
-    
-    def updateQ(self, state, action, reward, newValue):
-        currentQ = self.q.get((state, action), None)
-        if currentQ is None:
-            self.q[(state, action)] = reward 
-        else:
-            self.q[(state, action)] = currentQ + self.alpha * (newValue - currentQ)
-    
-    def learn(self, state1, action1, reward, state2, action2):
-        pass
-        
-    def act(self, state):
-        if np.random.rand() < epsilon:
+# coding: utf-8
+
+# In[1]:
+
+import gym
+import agents
+import numpy as np
+import random
+
+env = gym.make('Taxi-v2')
+agent = agents.SarsaAgent(actions = env.action_space.n )#alpha, gamma = #gamma, ...)
+
+state = env.reset()
+done = False
+episodes = 100#episodes
+steps = 0
+totalSteps = 0
+
+for episode in range(episodes):
+
+    while not done:
+        if np.random.rand() < agent.epsilon:
             action = env.action_space.sample()
         else:
-            q = [self.getQ(state, a) for a in self.actions] #fix this line - done
-            maxQ = max(q)
-            count = q.count(maxQ)
-            if count > 1:
-                best = [a for a in self.actions if q[a] == maxQ] #fix this line
-                i = random.choice(best)
-            else:
-                i = q.index(maxQ)
+            action = agent.act(state) #do this for next action
+        next_state, reward, done, info = env.step(action)
+        agent.learn(state, action, reward, next_state          )               #next_action)
+        state = next_state
 
-            action = self.actions[i]
-        return action
+        #put in next_action
 
-class sarsa(TD):
-    
-    def learn(self, state1, action1, reward, state2, action2):
-        nextQ = self.getQ(state2, action2)
-        self.updateQ(state1, action1, reward, reward + gamma * nextQ)
+        steps += 1
+        print(steps)
 
-class q_learn(TD):
-    
-    def learn(self, state1, action1, reward, state2):
-        maxQ = max([self.getQ(state2, a) for a in self.actions])
-        self.updateQ(state1, action1, reward, reward + gamma * maxQ)
+    totalSteps += steps
 
-class expected_sarsa(TD):
-    
-    def learn(self, state1, action1, reward, state2):
-        meanQ = mean([self.getQ(state2, a) for a in self.actions])
-        self.updateQ(state1, action1, reward, reward + gamma * meanQ)
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
